082011a46d37e88a6daa5a53a2a3e60c8a9da82a
Edward <edwardguerrero15@gmail.com>
2021-03-05 19:00:32 -0800

    Crud Fortunes api created

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const fs = require('fs')
 
 const app = express();
-const fortunes = require('./data/fortunes.json')
+const fortunes = require('./data/fortunes.json');
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/fortunes',(req,res)=>{
     res.json(fortunes)
@@ -20,21 +20,48 @@ app.get('/fortunes/:id',(req,res)=>{
 
 })
 
+const writeFortunes = (json) =>{
+    fs.writeFile("./data/fortunes.json",JSON.stringify(json), err => console.log(err));
+}
+
 app.post('/fortunes',(req,res)=>{
-    console.log(req.body)
 
     const {message, luckyNumber, spiritAnimal} = req.body
     const fortuneIds = fortunes.map(obj => obj.id);
 
-    const fortune = {
-        id: (fortuneIds.length > 0 ?Math.max(...fortuneIds) : 0) +1,
+    
+    const newFortunes = fortunes.concat({
+        id: (fortuneIds.length > 0 ? Math.max(...fortuneIds) : 0) +1,
                 message,
             luckyNumber,
-          spiritAnimal};
+          spiritAnimal});
+    writeFortunes(newFortunes)
     
-    const newFortunes = fortunes.concat(fortune);
-    fs.writeFile("./data/fortunes.json",JSON.stringify(newFortunes), err => console.log(err));
     res.json(newFortunes)
+})
+
+app.put('/fortunes/:id', (req,res) => {
+    const {id} = req.params;
+    const {message, luckyNumber, spiritAnimal} = req.body;
+    const oldFortune = fortunes.find(f => f.id == id);
+
+
+    ['message', 'luckyNumber', 'spiritAnimal'].forEach(key =>{
+        if(req.body[key]) oldFortune[key] = req.body[key];
+    })
+
+
+
+
+    writeFortunes(fortunes);
+    res.json(fortunes);
+})
+
+app.delete('/fortunes/:id',(req,res)=>{
+    const {id} = req.params;
+    const newFortunes = fortunes.filter(f => f.id != id);
+    writeFortunes(newFortunes);
+    res.json(newFortunes);
 })
 
 module.exports = app
